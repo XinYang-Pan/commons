972892d853e415005d8f46628bd849076bc26f12
Pan, Xinyang <seanpan2008@gmail.com>
2017-05-11 14:20:09 +0800

    Signed-off-by: Pan, Xinyang <seanpan2008@gmail.com>

---
+++ src/main/java/io/github/xinyangpan/commons/FormatterWrapper.java
@@ -0,0 +1,147 @@
+package io.github.xinyangpan.commons;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.Flushable;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Formatter;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.io.Files;
+
+public class FormatterWrapper implements Closeable, Flushable {
+	private static final String DEFAULT_PREFIX = "\t";
+	private final Formatter formatter;
+	private final String prefix;
+	private final int defaultPrefixRepeat;
+
+	// --------------------------------------
+	// ---- Constructors
+	// --------------------------------------
+
+	public FormatterWrapper() {
+		this(DEFAULT_PREFIX);
+	}
+
+	public FormatterWrapper(String prefix) {
+		this(new Formatter(), prefix);
+	}
+
+	public FormatterWrapper(Formatter formatter) {
+		this(formatter, DEFAULT_PREFIX, 0);
+	}
+
+	public FormatterWrapper(Formatter formatter, String prefix) {
+		this(formatter, prefix, 0);
+	}
+
+	public FormatterWrapper(Formatter formatter, int defaultPrefixRepeat) {
+		this(formatter, DEFAULT_PREFIX, defaultPrefixRepeat);
+	}
+
+	public FormatterWrapper(Formatter formatter, String prefix, int defaultPrefixRepeat) {
+		super();
+		this.formatter = formatter;
+		this.prefix = prefix;
+		this.defaultPrefixRepeat = defaultPrefixRepeat;
+	}
+
+	// -----------------------------
+	// ----- Static Methods
+	// -----------------------------
+
+	public static Formatter createFormatter(String filePath) {
+		try {
+			File file = new File(filePath);
+			Files.createParentDirs(file);
+			file.createNewFile();
+			return new Formatter(file);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	// --------------------------------------
+	// ---- messageformat
+	// --------------------------------------
+
+	public FormatterWrapper messageformat(String format, Object... args) {
+		return this.messageformat(0, format, args);
+	}
+
+	public FormatterWrapper messageformat(int prefixRepeat, String format, Object... args) {
+		return this.messageformat(prefixRepeat, false, format, args);
+	}
+
+	public FormatterWrapper messageformatln(String format, Object... args) {
+		return this.messageformatln(0, format, args);
+	}
+
+	public FormatterWrapper messageformatln(int prefixRepeat, String format, Object... args) {
+		return this.messageformat(prefixRepeat, true, format, args);
+	}
+
+	public FormatterWrapper messageformat(int prefixRepeat, boolean newLine, String format, Object... args) {
+		return this.format(prefixRepeat, true, MessageFormat.format(format, args));
+	}
+
+	// --------------------------------------
+	// ---- format
+	// --------------------------------------
+
+	public FormatterWrapper format(String format, Object... args) {
+		return this.format(0, format, args);
+	}
+
+	public FormatterWrapper format(int prefixRepeat, String format, Object... args) {
+		return this.format(prefixRepeat, false, format, args);
+	}
+
+	public FormatterWrapper formatln() {
+		return this.formatln(0 - defaultPrefixRepeat, "");
+	}
+
+	public FormatterWrapper formatln(String format, Object... args) {
+		return this.formatln(0, format, args);
+	}
+
+	public FormatterWrapper formatln(int prefixRepeat, String format, Object... args) {
+		return this.format(prefixRepeat, true, format, args);
+	}
+
+	public FormatterWrapper format(int prefixRepeat, boolean newLine, String format, Object... args) {
+		prefixRepeat = prefixRepeat + defaultPrefixRepeat;
+		if (prefixRepeat > 0) {
+			formatter.format("%s", StringUtils.repeat(prefix, prefixRepeat));
+		}
+		if (StringUtils.isNotEmpty(format)) {
+			formatter.format(format, args);
+		}
+		if (newLine) {
+			formatter.format("%n");
+		}
+		return this;
+	}
+
+	@Override
+	public void close() {
+		Appendable out = formatter.out();
+		// NEVER CLOSE System.out
+		if (out == null || out == System.out) {
+			return;
+		}
+		formatter.close();
+	}
+
+	@Override
+	public void flush() {
+		formatter.flush();
+	}
+
+	@Override
+	public String toString() {
+		return formatter.toString();
+	}
+}
